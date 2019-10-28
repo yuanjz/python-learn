c6d7f5838002df692ebad900d70ce4ad3836f6f6
yuanjz <yuanjz@asiainfo-sec.com>
2019-10-28 17:11:22 +0800

    类

---
+++ yuan/Fruit.py
@@ -0,0 +1,20 @@
+class Fruit():
+    def __init__(self):
+        print ("Fruit")
+    def run_base(self):
+        print ("Fruit run_base")
+
+class Apple(Fruit):
+    def __init__(self):
+        print ("Apple")
+    def run_base(self):
+        print ("Apple run_base")
+    def run_sub(self):
+        print ("Apple run_sub")
+
+fruit = Fruit()
+fruit.run_base()
+
+apple = Apple()
+apple.run_base()
+apple.run_sub()
---
+++ yuan/list.py
@@ -0,0 +1,4 @@
+list_a = [1, 2, 3]
+print(list_a)
+list_a.append(4)
+print(list_a)
